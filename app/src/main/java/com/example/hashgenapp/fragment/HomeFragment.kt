5a4af501a5dd289ba9a26c20581fed395caa1255
UsmanFani <usmanfani86@gmail.com>
2022-01-27 00:31:41 +0530

    added Adapter

--- app/src/main/java/com/example/hashgenapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/hashgenapp/fragment/HomeFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.example.hashgenapp.R
+import com.example.hashgenapp.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
-
+class HomeFragment : Fragment(R.layout.fragment_home),AdapterView.OnItemSelectedListener {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,7 +20,26 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val spinner = binding.appCompatSpinner
+        spinner.adapter = ArrayAdapter.createFromResource(
+            this.requireContext(),
+            R.array.hashArray,
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spinner.onItemSelectedListener=this
+
+        return binding.root
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+       val item= parent?.getItemAtPosition(pos).toString()
+        Toast.makeText(context,item,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/example/hashgenapp/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/hashgenapp/fragment/ResultFragment.kt
@@ -6,19 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.hashgenapp.R
+import com.example.hashgenapp.databinding.FragmentResultBinding
 
 
 class ResultFragment : Fragment(R.layout.fragment_result) {
+    private var _binding:FragmentResultBinding?=null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding= FragmentResultBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 }
